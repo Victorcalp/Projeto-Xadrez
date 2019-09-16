166fe84224acf4b2a0abc69b515de2f28b025050
Victorcalp <victorcalp@outlook.com.br>
2019-09-16 19:05:07 -0300

    criado classes tabuleiro

---
+++ Projeto Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,12 @@
+﻿namespace Tabuleiro
+{
+    class Peca
+    {
+
+        public int QtdMovimentos { get; set; }
+        public Cor cor { get; protected set; }
+        public Posicao Posicao { get; set; }
+        public Tabuleiros tabu { get; set; }
+
+    }
+}
